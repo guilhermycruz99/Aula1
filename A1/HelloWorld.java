c6d4c786208644676f97a3ea21bc6f07547d9aed
Guilhermy <guilhermy.cruz749@gmail.com>
2026-08-11 21:12:34 -0300

    Meu primeiro código
    Primeira aula de Algoritmos e Programação.

---
+++ A1/HelloWorld.java
@@ -0,0 +1,8 @@
+    public class HelloWorld {
+        public static void main(String[] args) {
+            System.out.println("Hello World!");  
+            System.out.println("Suco de Laranja");
+            System.out.println(" Suco de Acerola");
+            System.out.println("Suco de Maracujá");
+        }
+    }
---
+++ A1/hello.py
@@ -0,0 +1,4 @@
+print("Hello, World!")
+print("Meu nome é Guilhermy")
+print("Sou p2 de Ciências da Computação")
+print("Não conheço tanto sobre programação, mas me interesso pela área de cyber security e hacking.")
